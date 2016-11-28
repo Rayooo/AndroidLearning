d015d44f23a8187d9fa96afb36bc2533a018d26c
Ray <chenxiaolei.ray@gmail.com>
2016-11-28 14:18:46 +0800

    完成删除学生功能

--- Lab6/app/src/main/java/com/ray/lab6/EditStudentInfoActivity.java
+++ Lab6/app/src/main/java/com/ray/lab6/EditStudentInfoActivity.java
@@ -1,5 +1,6 @@
 package com.ray.lab6;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class EditStudentInfoActivity extends AppCompatActivity {
 
@@ -57,7 +59,44 @@ public class EditStudentInfoActivity extends AppCompatActivity {
 
 
     public void modify(View v){
+        String name = ((EditText)findViewById(R.id.nameEditText)).getText().toString();
+        String code = ((EditText)findViewById(R.id.codeEditText)).getText().toString();
 
+        RadioGroup rg = (RadioGroup)findViewById(R.id.radioGroup);
+        String sex = ((RadioButton)this.findViewById(rg.getCheckedRadioButtonId())).getText().toString();
+
+        String mobile = ((EditText)findViewById(R.id.mobileEditText)).getText().toString();
+        String address = ((EditText)findViewById(R.id.addressEditText)).getText().toString();
+
+        if(name.isEmpty() || code.isEmpty() || mobile.isEmpty() || address.isEmpty()){
+            Toast.makeText(this, "请填写完整所有内容", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            DatabaseHelper databaseHelper = new DatabaseHelper(this);
+            SQLiteDatabase db = databaseHelper.getWritableDatabase();
+
+            Cursor cursor = db.rawQuery("SELECT * FROM student WHERE code=?",new String[]{code});
+            if (cursor.moveToNext() && !cursor.getString(cursor.getColumnIndex("code")).equals(rawCode)){
+                Toast.makeText(this, "存在重复学号，请重新输入学号", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                ContentValues contentValues = new ContentValues();
+                contentValues.put("name", name);
+                contentValues.put("code", code);
+                contentValues.put("sex", sex);
+                contentValues.put("mobile", mobile);
+                contentValues.put("address", address);
+                db.update("student", contentValues,"code=?",new String[]{rawCode});
+                Toast.makeText(this, "修改成功", Toast.LENGTH_SHORT).show();
+
+                //返回
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("from", "editStudent");
+                setResult(RESULT_OK,returnIntent);
+                finish();
+            }
+            cursor.close();
+        }
     }
 
     public void deleteStudent(View v){
